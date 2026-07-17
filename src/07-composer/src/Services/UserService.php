e589c34148bb1b39c99a81fcd3d81ed80f4657ef
Edwin Alexander Bernardino Moran <edwinalexanderbernardinomoran@gmail.com>
2026-07-17 16:32:09 -0600

    S8 - NameSpaces

---
+++ src/07-composer/src/Services/UserService.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+
+class UserService
+{
+    public function create(User $user): void
+    {
+        // Logic to create a user in the database
+        echo "User {$user->name} with email {$user->email} has been created.";
+    }
+}
